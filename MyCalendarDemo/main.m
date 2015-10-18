239dac947de50e3c6db7257a3c63d871509a6ba7
chenyufeng <503434355@qq.com>
2015-10-18 20:43:07 +0800

    Initial Commit

---
+++ MyCalendarDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MyCalendarDemo
+//
+//  Created by chenyufeng on 15/10/18.
+//  Copyright © 2015年 chenyufengweb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+    return 0;
+}
